61804c715dc370a1a740f8e60be06f88ee19a012
Sahir Syatha <sahirsyatha77@gmail.com>
2021-05-05 00:09:58 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/academies/ui/academy/AcademyFragment.kt
@@ -0,0 +1,41 @@
+package com.dicoding.academies.ui.academy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.academies.utils.DataDummy
+import com.dicoding.academies.viewmodel.ViewModelFactory
+import com.dicoding.academy.databinding.FragmentAcademyBinding
+
+class AcademyFragment : Fragment() {
+
+    private lateinit var fragmentAcademyBinding: FragmentAcademyBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        fragmentAcademyBinding = FragmentAcademyBinding.inflate(layoutInflater, container, false)
+        return fragmentAcademyBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (activity != null) {
+            val factory = ViewModelFactory.getInstance(requireActivity())
+            val viewModel = ViewModelProvider(this, factory)[AcademyViewModel::class.java]
+            val courses = viewModel.getCourses()
+
+            val academyAdapter = AcademyAdapter()
+            academyAdapter.setCourses(courses)
+
+            with(fragmentAcademyBinding.rvAcademy) {
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                adapter = academyAdapter
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dicoding/academies/ui/home/HomeActivity.kt
@@ -0,0 +1,23 @@
+package com.dicoding.academies.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.academy.R
+import com.dicoding.academy.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val activityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(activityHomeBinding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        activityHomeBinding.viewPager.adapter = sectionsPagerAdapter
+        activityHomeBinding.tabs.setupWithViewPager(activityHomeBinding.viewPager)
+
+        supportActionBar?.elevation = 0f
+
+    }
+}

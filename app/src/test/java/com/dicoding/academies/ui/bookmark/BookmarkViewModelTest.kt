1a3bdd37b52e1e77c110905038c84edd7a00de3c
Sahir <sahirsyatha77@gmail.com>
2021-05-05 15:46:15 +0700

    testing repo
    testing

--- app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.kt
+++ app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.kt
@@ -1,20 +1,35 @@
 package com.dicoding.academies.ui.bookmark
 
+import com.dicoding.academies.data.AcademyRepository
+import com.dicoding.academies.data.source.local.entity.CourseEntity
+import com.dicoding.academies.utils.DataDummy
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class BookmarkViewModelTest {
     private lateinit var viewModel: BookmarkViewModel
 
+    @Mock
+    private lateinit var academyRepository: AcademyRepository
+
     @Before
     fun setUp() {
-        viewModel = BookmarkViewModel()
+        viewModel = BookmarkViewModel(academyRepository)
     }
 
     @Test
     fun getBookmark() {
+        `when`(academyRepository.getBookmarkedCourses()).thenReturn(DataDummy.generateDummyCourses() as ArrayList<CourseEntity>)
         val courseEntities = viewModel.getBookmarks()
+        verify(academyRepository).getBookmarkedCourses()
         assertNotNull(courseEntities)
         assertEquals(5, courseEntities.size)
     }

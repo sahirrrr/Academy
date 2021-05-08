4e01ff1d4aae3aa5add3a6d22e48472269a38011
Sahir <sahirsyatha77@gmail.com>
2021-05-08 11:08:29 +0700

    testing livedata

--- app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.kt
+++ app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.kt
@@ -1,24 +1,35 @@
 package com.dicoding.academies.ui.bookmark
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.dicoding.academies.data.AcademyRepository
 import com.dicoding.academies.data.source.local.entity.CourseEntity
 import com.dicoding.academies.utils.DataDummy
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.*
+import kotlin.collections.ArrayList
 
 @RunWith(MockitoJUnitRunner::class)
 class BookmarkViewModelTest {
     private lateinit var viewModel: BookmarkViewModel
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
     @Mock
     private lateinit var academyRepository: AcademyRepository
+
+    @Mock
+    private lateinit var observer: Observer<List<CourseEntity>>
 
     @Before
     fun setUp() {
@@ -27,10 +38,17 @@ class BookmarkViewModelTest {
 
     @Test
     fun getBookmark() {
-        `when`(academyRepository.getBookmarkedCourses()).thenReturn(DataDummy.generateDummyCourses() as ArrayList<CourseEntity>)
-        val courseEntities = viewModel.getBookmarks()
+        val dummyCourses = DataDummy.generateDummyCourses()
+        val courses = MutableLiveData<List<CourseEntity>>()
+        courses.value = dummyCourses
+
+        `when`(academyRepository.getBookmarkedCourses()).thenReturn(courses)
+        val courseEntities = viewModel.getBookmarks().value
         verify(academyRepository).getBookmarkedCourses()
         assertNotNull(courseEntities)
-        assertEquals(5, courseEntities.size)
+        assertEquals(5, courseEntities?.size)
+
+        viewModel.getBookmarks().observeForever(observer)
+        verify(observer).onChanged(dummyCourses)
     }
 }
--- app/src/test/java/com/dicoding/academies/ui/reader/CourseReaderViewModelTest.kt
+++ app/src/test/java/com/dicoding/academies/ui/reader/CourseReaderViewModelTest.kt
@@ -1,5 +1,8 @@
 package com.dicoding.academies.ui.reader
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.dicoding.academies.data.AcademyRepository
 import com.dicoding.academies.data.source.local.entity.ContentEntity
 import com.dicoding.academies.data.source.local.entity.ModuleEntity
@@ -8,6 +11,7 @@ import org.junit.Before
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
@@ -24,8 +28,17 @@ class CourseReaderViewModelTest {
     private val dummyModules = DataDummy.generateDummyModules(courseId)
     private val moduleId = dummyModules[0].moduleId
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
     @Mock
     private lateinit var academyRepository: AcademyRepository
+
+    @Mock
+    private lateinit var modulesObserver: Observer<List<ModuleEntity>>
+
+    @Mock
+    private lateinit var moduleObserver: Observer<ModuleEntity>
 
     @Before
     fun setUp() {
@@ -39,17 +52,26 @@ class CourseReaderViewModelTest {
 
     @Test
     fun getModules() {
-        `when`(academyRepository.getAllModulesByCourse(courseId)).thenReturn(dummyModules)
-        val moduleEntities = viewModel.getModules()
+        val modules = MutableLiveData<List<ModuleEntity>>()
+        modules.value = dummyModules
+
+        `when`(academyRepository.getAllModulesByCourse(courseId)).thenReturn(modules)
+        val moduleEntities = viewModel.getModules().value
         verify(academyRepository).getAllModulesByCourse(courseId)
         assertNotNull(moduleEntities)
-        assertEquals(7, moduleEntities.size.toLong())
+        assertEquals(7, moduleEntities?.size)
+
+        viewModel.getModules().observeForever(modulesObserver)
+        verify(modulesObserver).onChanged(dummyModules)
     }
 
     @Test
     fun getSelectedModule() {
-        `when`(academyRepository.getContent(courseId, moduleId)).thenReturn(dummyModules[0])
-        val moduleEntity = viewModel.getSelectedModule()
+        val module = MutableLiveData<ModuleEntity>()
+        module.value = dummyModules[0]
+
+        `when`(academyRepository.getContent(courseId, moduleId)).thenReturn(module)
+        val moduleEntity = viewModel.getSelectedModule().value as ModuleEntity
         verify(academyRepository).getContent(courseId, moduleId)
         assertNotNull(moduleEntity)
 
@@ -59,5 +81,8 @@ class CourseReaderViewModelTest {
         val content = contentEntity?.content
         assertNotNull(content)
         assertEquals(content, dummyModules[0].contentEntity?.content)
+
+        viewModel.getSelectedModule().observeForever(moduleObserver)
+        verify(moduleObserver).onChanged(dummyModules[0])
     }
 }
